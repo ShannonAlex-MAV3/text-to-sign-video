f43a4f7c45e0e639f3e4178e1e5f778b7254b35d
ShannonAlex-MAV3 <shannonakmeemana11@gmail.com>
2024-03-13 05:41:00 +0530

    multipart video stream previewed using html img tag, ahve to work on caching and looping the sequence

--- static/script.js
+++ static/script.js
@@ -1,27 +1,34 @@
-// Get the image element
 const videoElement = document.getElementById('video-stream');
 
 // Function to update the image source with the video stream
 function updateVideoStream() {
-    fetch('http://127.0.0.1:5000/video')  // Fetch the video stream from the /video endpoint
+    fetch('http://127.0.0.1:5000/video') 
         .then(response => {
             // console.log(response)
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-            let blb = response.blob();
             
-            return blb;  // Convert the response to a Blob
+            
+            return response.blob();  // Convert the response to a Blob
         })
         .then(blob => {
-            console.log(blob)
+            
             const blobUrl = URL.createObjectURL(blob);  // Create a URL for the Blob
+            
             videoElement.src = blobUrl;  // Set the image source to the Blob URL
+            console.log(blobUrl)
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
         });
 }
 
-// Call the updateVideoStream function to start streaming the video
 updateVideoStream();
+
+// let img = document.getElementById('stream');
+
+// img.onerror = function() {
+//     img.src = '';
+//     setTimeout(() => img.src = 'http://127.0.0.1:5000/video', 1000);
+// };
